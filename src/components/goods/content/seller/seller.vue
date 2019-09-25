875f8672f53400ab09372698e9948d9af89f424f
董伟超 <dongweichao@cescit.com>
2019-09-26 00:09:22 +0800

    seller组件

--- src/components/goods/content/seller/seller.vue
+++ src/components/goods/content/seller/seller.vue
@@ -1,21 +1,59 @@
 <template>
   <div>
-    hhh
+    <div class="seller">
+      <div class="seller-content">
+        <div class="overview">
+          <h1 class="title">{{seller.name}}</h1>
+          <div class="desc border-1px">
+            <star :size="36" :score="seller.score"></star>
+            <span class="text">({{seller.ratingCount}})</span>
+            <span class="text">月售{{seller.sellCount}}单</span>
+          </div>
+          <ul class="remake">
+            <li class="block">
+              <h2>起送价</h2>
+              <div class="content">
+                <span class="stress">{{seller.minPrice}}</span>元
+              </div>
+            </li>
+            <li class="block">
+              <h2>商家配送</h2>
+              <div class="content">
+                <span class="stress">{{seller.deliveryPrice}}</span>元
+              </div>
+            </li>
+            <li class="block">
+              <h2>平均配送时间</h2>
+              <div class="content">
+                <span class="stress">{{seller.deliveryTime}}</span>分钟
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script type='text/ecmascript-6'>
+import star from "components/goods/star/star"
 
   export default {
     name:'',
-    props:[''],
+    props:{
+      seller:{
+        type:Object
+      }
+    },
     data () {
       return {
 
       };
     },
 
-    components: {},
+    components: {
+      star
+    },
 
     computed: {},
 
@@ -31,5 +69,5 @@
 
 </script>
 <style lang='stylus' rel='stylesheet/stylus'>
-
+@import './seller'
 </style>
